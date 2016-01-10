065d7123530e3a8f0384fee61945397becd01fe2
Rasmus Bååth <rasmus.baath@gmail.com>
2016-01-10 02:57:27 +0100

    Refactoring

--- R/bayesboot.R
+++ R/bayesboot.R
@@ -1,3 +1,32 @@
+#' Produce random draws from a uniform Dirichlet distribution
+#'
+#' \code{rudirichlet} produces \code{n} draws from a \code{d}-dimensional uniform Dirichlet distribution.
+#' Here "uniform" implies that any combination of values on the support of the distribution is equally likely,
+#' that is, the \eqn{\alpha} parameters to the Dirichlet distribution are all set to 1.0.
+#'
+#'
+#' @param n the number of draws.
+#' @param d the dimension of the Dirichlet distribution.
+#'
+#' @return An \code{n} by \code{d} matrix.
+#' @export
+#'
+#' @examples
+#' rudirichlet(3, 3)
+#' # Should produce the following matrix, but with different numbers, of course:
+#' #        [,1]     [,2]   [,3]
+#' # [1,] 0.7109 0.001012 0.2881
+#' # [2,] 0.2112 0.491784 0.2970
+rudirichlet <- function(n, d) {
+  # Using the facts that you can transform gamma distributed draws into
+  # Dirichlet draws and that rgamma(n, 1) <==> rexp(n, 1)
+  # See here for explanation:
+  # https://en.wikipedia.org/wiki/Dirichlet_distribution#Random_number_generation#Gamma_distribution
+  rexp_mat <- matrix( rexp(d * n, 1) , ncol = d, byrow = TRUE)
+  dirichlet_weights <- rexp_mat / rowSums(rexp_mat)
+  dirichlet_weights
+}
+
 
 # Performs a Bayesian bootstrap and returns a sample of size n1 representing the
 # posterior distribution of the statistic. Returns a vector if the statistic is
@@ -8,62 +37,44 @@
 #   statistic A function that accepts data as its first argument and possibly
 #             the weights as its second, if use_weights is TRUE.
 #             Should return a numeric vector.
-#   n1        The size of the bootstrap sample.
-#   n2        The sample size used to calculate the statistic each bootstrap draw.
-#   use_weights  Whether the statistic function accepts a weight argument or
+#   R        The size of the bootstrap sample.
+#   R2       The sample size used to calculate the statistic each bootstrap draw.
+#   use.weights  Whether the statistic function accepts a weight argument or
 #                should be calculated using resampled data.
-#   weight_arg   If the statistic function includes a named argument for the
+#   weight.arg   If the statistic function includes a named argument for the
 #                weights this could be specified here.
 #   ...       Further arguments passed on to the statistic function.
+bayesboot <- function(data, statistic, R = 1000, R2 = 1000, use.weights = FALSE, weight.arg = NULL, ...) {
+  # Pick out the first part of statistic matching a legal variable name,
+  # just to be used as a label when plotting later.
+  statistic.label <- deparse(substitute(statistic))
+  match <- regexpr("^(\\w|\\.|_)*", "function <- fun")
+  statistic.label <- regmatches(statistic.label, match)
 
+  # Todo: Add a lot of error checks...
 
+  dirichlet_weights <- rudirichlet(R, NROW(data))
 
-#' bayesboot
-#'
-#' @param data
-#' @param statistic
-#' @param n1
-#' @param n2
-#' @param use_weights
-#' @param weight_arg
-#' @param ...
-#'
-#' @return
-#' @export
-#'
-#' @examples
-bayesboot <- function(data, statistic, n1 = 1000, n2 = 1000 , use_weights = FALSE, weight_arg = NULL, ...) {
-  # Draw from a uniform Dirichlet dist. with alpha set to rep(1, n_dim).
-  # Using the facts that you can transform gamma distributed draws into
-  # Dirichlet draws and that rgamma(n, 1) <=> rexp(n, 1)
-  dirichlet_weights <- matrix( rexp(NROW(data) * n1, 1) , ncol = NROW(data), byrow = TRUE)
-  dirichlet_weights <- dirichlet_weights / rowSums(dirichlet_weights)
-
-  if(use_weights) {
-    stat_call <- quote(statistic(data, w, ...))
-    names(stat_call)[3] <- weight_arg
-    boot_sample <- apply(dirichlet_weights, 1, function(w) {
+  if(use.weights) {
+    stat_call <- quote(statistic(data, weights, ...))
+    names(stat_call)[3] <- weight.arg
+    boot_sample <- adply(dirichlet_weights, 1, .id = NULL, function(weights) {
       eval(stat_call)
     })
   } else {
-    if(is.null(dim(data)) || length(dim(data)) < 2) { # data is a list type of object
-      boot_sample <- apply(dirichlet_weights, 1, function(w) {
-        data_sample <- sample(data, size = n2, replace = TRUE, prob = w)
+    if(is.atomic(x) || is.list(x)) { # data is a list type of object
+      boot_sample <- adply(dirichlet_weights, 1, .id = NULL, function(weights) {
+        data_sample <- sample(data, size = R2, replace = TRUE, prob = weights)
         statistic(data_sample, ...)
       })
-    } else { # data is a table type of object
-      boot_sample <- apply(dirichlet_weights, 1, function(w) {
-        index_sample <- sample(nrow(data), size = n2, replace = TRUE, prob = w)
+    } else { # assume data can be subsetted like a matrix or data.frame
+      boot_sample <- adply(dirichlet_weights, 1, .id = NULL, function(weights) {
+        index_sample <- sample(nrow(data), size = R2, replace = TRUE, prob = weights)
         statistic(data[index_sample, ,drop = FALSE], ...)
       })
     }
   }
-  if(is.null(dim(boot_sample)) || length(dim(boot_sample)) < 2) {
-    # If the bootstrap sample is just a simple vector return it.
-    boot_sample
-  } else {
-    # Otherwise it is a matrix. Since apply returns one row per statistic
-    # let's transpose it and return it as a data frame.
-    as.data.frame(t(boot_sample))
-  }
+  class(boot_sample) <- c("bayesboot", class(boot_sample))
+  attr(boot_sample, "statistic.label") <- statistic.label
+  boot_sample
 }
--- tests/testthat/test-bayesboot.R
+++ tests/testthat/test-bayesboot.R
@@ -1,1 +1,6 @@
 context("Bayesian bootstrap")
+
+test_that("rudirichlet produces a valid output", {
+  rand_mat <- rudirichlet(10, 15)
+  expect_equivalent(rowSums(rand_mat), rep(1, 10))
+})
